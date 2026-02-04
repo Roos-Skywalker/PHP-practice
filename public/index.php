37675c70ecfea891516380c6aa2ac1b0c6cac1e0
Roos-Skywalker <grifioen@live.nl>
2026-02-04 12:19:10 +0100

    Index.php updated to work with Routers

--- public/index.php
+++ public/index.php
@@ -5,11 +5,12 @@ use Framework\Kernel;
 use Framework\Request;
 use Framework\Response;
 // Use phpinfo(); for a nice debug and show global PHP variables.
-
-
-echo "Hello, PHP!";
-
 $kernel = new Kernel();
+
+// Define routes
+$router = $kernel->getRouter();
+$router->addRoute("GET", "/", "Routing until I true");
+
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //This variable ONLY contains the path.
 if (!is_string($urlPath)) {
     $urlPath = '/';
