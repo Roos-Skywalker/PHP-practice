8aef61603223c5f17af89f71b2c447127269b97d
Roos-Skywalker <grifioen@live.nl>
2026-02-04 11:03:38 +0100

    Route.php

--- src/Route.php
+++ src/Route.php
@@ -4,5 +4,19 @@ namespace Framework;
 
 class Route
 {
+    private string $method;
+    private string $path;
+    public string $return;
 
+    public function __construct(string $method, string $path, string $return)
+    {
+        $this->method = $method;
+        $this->path = $path;
+        $this->return = $return;
+    }
+
+    public function matches($method, $path): bool
+    {
+        return $this->method === $method && $this->path === $path;
+    }
 }
