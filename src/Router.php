80a5e6213a8b9404e9fff906b2ab688601aeddaf
Roos-Skywalker <grifioen@live.nl>
2026-02-04 11:17:50 +0100

    Router.php

--- src/Router.php
+++ src/Router.php
@@ -4,5 +4,31 @@ namespace Framework;
 
 class Router
 {
+    /** @var array<Route> */
+    public array $routes;
+    public function __construct()
+    {
+    }
+
+    public function dispatch(Request $request): Response
+    {
+        $matchedRoute = null;
+        foreach ($this->routes as $route) {
+            if ($route->matches($request->method, $request->path)) {
+                $matchedRoute = $route;
+                break;
+            }
+        }
+        if ($matchedRoute === null) {
+            // Route not found error 404.
+            $response = new Response("Page not found", 404);
+        }
+        return new Response($matchedRoute->return);
+    }
+
+    public function addRoute($method, $path, $return): void
+    {
+        $this->routes[] = new Route($method, $path, $return);
+    }
 
 }
