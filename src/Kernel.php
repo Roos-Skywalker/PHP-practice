b562dd299439c48a4ff008e4a31f76e46b6fc5ef
Roos-Skywalker <grifioen@live.nl>
2026-02-04 12:17:12 +0100

    Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,14 +4,21 @@ namespace Framework;
 
 class Kernel
 {
+    private Router $router;
     public function __construct()
     {
+        $this->router = new Router();
     }
 
+    public function getRouter(): Router
+    {
+        return $this->router;
+    }
     public function handle(Request $request): Response
     {
-        $queryParameterString = implode(',', $request->queryParameters); //Turns a string array into a single string with , to separate.
-        $response = new Response(body: "$queryParameterString, $request->path"); //Double quotes let you place variables.
-        return $response;
+//        $queryParameterString = implode(',', $request->queryParameters); //Turns a string array into a single string with , to separate.
+//        $response = new Response(body: "$queryParameterString, $request->path"); //Double quotes let you place variables.
+        return $this->router->dispatch($request);
+//        return $response;
     }
 }
