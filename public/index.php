a1ebfa2e4082c071788d408977a7ec8c6edd409c
Roos-Skywalker <grifioen@live.nl>
2026-02-05 09:42:54 +0100

    FrontController had TaskController routes

--- public/index.php
+++ public/index.php
@@ -3,17 +3,22 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\HomeController;
+use App\Controllers\TaskController;
 use Framework\Kernel;
 use Framework\Request;
 use Framework\Response;
 // Use phpinfo(); for a nice debug and show global PHP variables.
 $kernel = new Kernel();
 $homeController = new HomeController();
+$taskController = new TaskController();
 
 // Define routes
 $router = $kernel->getRouter();
 $router->addRoute("GET", "/", [$homeController, "index"]);
 $router->addRoute("GET", "/about", [$homeController, "about"]);
+
+$router->addRoute("GET", "/", [$taskController, "index"]);
+$router->addRoute("GET", "/about", [$taskController, "create"]);
 
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //This variable ONLY contains the path.
 if (!is_string($urlPath)) {
