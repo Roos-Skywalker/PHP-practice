ff5cd5ae7402f2e88cb9d612138d5d9a9a8d0d3a
Roos-Skywalker <grifioen@live.nl>
2026-02-17 11:55:32 +0100

    Unfinished index.php and ResponseFactory.php for Twig

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require_once '/path/to/vendor/autoload.php';
 
 use App\RouteProvider;
 use App\ServiceProvider;
--- src/ResponseFactory.php
+++ src/ResponseFactory.php
@@ -4,10 +4,23 @@ namespace Framework;
 
 class ResponseFactory
 {
+    private \Twig\Environment $twig;
+
+    public function __construct()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader('app/views');
+        $this->twig = new \Twig\Environment($loader);
+    }
     public function body(string $body): Response
     {
         $newBody = "<div style=color:green;>" . $body . "</div>";
         $response = new Response($newBody, 200);
+        return $response;
+    }
+
+    public function view(string $template, $parameters): Response
+    {
+        $response = new Response($this->twig->render($template, $parameters), 200);
         return $response;
     }
 
