4497a9cf04f690d5b8c544fe09b92be603982b21
Roos-Skywalker <grifioen@live.nl>
2026-03-03 18:51:48 +0100

    Added error message to ServiceProvider.php

--- app/ServiceProvider.php
+++ app/ServiceProvider.php
@@ -4,12 +4,16 @@ namespace App;
 
 use App\Controllers\HomeController;
 use App\Controllers\TaskController;
+use Exception;
 use Framework\ResponseFactory;
 use Framework\ServiceContainer;
 use Framework\ServiceProviderInterface;
 
 class ServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * @throws Exception
+     */
     public function register(ServiceContainer $container): void
     {
         $responseFactory = $container->get(ResponseFactory::class);
