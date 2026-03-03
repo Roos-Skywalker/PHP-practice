c87e45ad2a3ccfbcd9f6ef8a9bb9535d69c530f6
Roos-Skywalker <grifioen@live.nl>
2026-03-03 18:31:51 +0100

    Added comments and config variable to index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+// Autoload dependencies and classes
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\RouteProvider;
@@ -8,17 +9,32 @@ use Framework\Kernel;
 use Framework\Request;
 
 // Use phpinfo(); for a nice debug and show global PHP variables.
-$kernel = new Kernel();
+// Initialize the Kernel with configuration
+$config = array(
+    'APP_ENV' => 'development',
+    'VIEWS_PATH' => 'app/views'
+);
+
+$kernel = new Kernel($config);
+
+// Declare variables that contain an new instance of each class.
 $serviceProvider = new ServiceProvider();
 $routeProvider = new RouteProvider();
-
+// Tell the kernel to use its functions to call on the variables containing the classes to be loaded.
 $kernel->registerServices($serviceProvider);
 $kernel->registerRoutes($routeProvider);
 
+// Extract the path from the URL
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //This variable ONLY contains the path.
 if (!is_string($urlPath)) {
     $urlPath = '/';
 }
+
+// Create the Request object
 $request = new Request($_SERVER['REQUEST_METHOD'], $urlPath, $_GET, $_POST);
+
+// Handle the request and get the response
 $response = $kernel->handle($request);
+
+// Send the response to the client
 $response->echo();
